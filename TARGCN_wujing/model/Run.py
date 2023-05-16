396d32ffc71441da61653381c3075ab48f075548
jccc100 <2428791113@qq.com>
2023-05-16 13:52:27 +0800

    add D4

--- TARGCN_wujing/model/Run.py
+++ TARGCN_wujing/model/Run.py
@@ -95,6 +95,7 @@ else:
     args.device = 'cpu'
 
 #gen adj
+print("************",args.dataset)
 distance_file='../data/{}/distance.csv'.format(args.dataset)
 print(distance_file)
 def get_adjacent_matrix(distance_file: str, num_nodes: int, id_file: str = None, graph_type="distance") -> np.array:
