aff767a06a4411b78d86d1934f229837db146ba1
jccc100 <2428791113@qq.com>
2023-05-16 13:18:06 +0800

    add targcn

--- TARGCN_wujing/model/BasicTrainer.py
+++ TARGCN_wujing/model/BasicTrainer.py
@@ -213,12 +213,12 @@ class Trainer(object):
             y_pred = scaler.inverse_transform(torch.cat(y_pred, dim=0))
         np.save('./{}_true.npy'.format(args.dataset), y_true.cpu().numpy())
         np.save('./{}_pred.npy'.format(args.dataset), y_pred.cpu().numpy())
-        try:
-            # send_email('./{}_true.npy'.format(args.dataset))
-            # send_email('./{}_pred.npy'.format(args.dataset))
-        except Exception as e:
-            print(e)
-            print("send error!!!")
+        # try:
+        #     # send_email('./{}_true.npy'.format(args.dataset))
+        #     # send_email('./{}_pred.npy'.format(args.dataset))
+        # except Exception as e:
+        #     print(e)
+        #     print("send error!!!")
         for t in range(y_true.shape[1]):
             mae, rmse, mape, _, _ = All_Metrics(y_pred[:, t, ...], y_true[:, t, ...],
                                                 args.mae_thresh, args.mape_thresh)
